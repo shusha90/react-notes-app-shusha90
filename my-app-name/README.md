c85a6861c70cb0159788e81042c69ac6d4d849c1
Dmitrii Shushukin <139569636+shusha90@users.noreply.github.com>
2023-11-14 11:53:14 -0800

    Delete my-app-name directory

--- my-app-name/README.md
+++
@@ -1,5 +0,0 @@
-# Note App
-
-## Full stack App using React deploying to Netlify:
-
-## [https://noteappmaterialui.netlify.app/](https://noteappmaterialui.netlify.app/)
--- my-app-name/src/helpers/noteModel.js
+++
@@ -1,9 +0,0 @@
-const noteModel = (id, body, title = "No Title") => ({
-  [id]: {
-    title: title,
-    body: body,
-    date: Date.now(),
-  },
-});
-
-export default noteModel;
